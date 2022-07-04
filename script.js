6a02433b2279ec3fe9b641d55960413c55445cee
savanth ks <savanthks13055@gmail.com>
2022-07-04 15:48:54 +0530

    menu button error corrected

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
     });
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
-        $('.menu-btn').toggleClass("active");
+        // $('.menu-btn').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 });
